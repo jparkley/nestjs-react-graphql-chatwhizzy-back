bed47fa42c315862f286a163536ace93998a67dc
jparkley <parkleydondon@yahoo.com>
2024-04-28 15:36:02 -0700

    add validationpipe and custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
