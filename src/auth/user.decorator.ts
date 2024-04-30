56c30c7e5fef95b2f0dc30cf7c8d39280afa8e82
jparkley <parkleydondon@yahoo.com>
2024-04-29 20:02:34 -0700

    add jwt guard for graphql

--- src/auth/user.decorator.ts
+++ src/auth/user.decorator.ts
@@ -1,10 +1,15 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
   // Return current user of the request
   // that passsport automatically populated
   (_data: unknown, context: ExecutionContext): User => {
-    return context.switchToHttp().getRequest().user;
+    if (context.getType() === 'http') {
+      return context.switchToHttp().getRequest().user;
+    } else if (context.getType<GqlContextType>() === 'graphql') {
+      return GqlExecutionContext.create(context).getContext().req.user;
+    }
   },
 );
