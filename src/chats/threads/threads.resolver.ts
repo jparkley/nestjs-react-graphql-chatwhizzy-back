82b1cd02ce0db3c5f547e89eddb1965088a1a7ea
jparkley <parkleydondon@yahoo.com>
2024-05-17 20:15:12 -0700

    add get all threads for chosen chat

--- src/chats/threads/threads.resolver.ts
+++ src/chats/threads/threads.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { ThreadsService } from './threads.service';
 import { Thread } from './entities/thread.entity';
 import { UseGuards } from '@nestjs/common';
@@ -6,6 +6,7 @@ import { GqlGuard } from 'src/auth/guards/gql-guard';
 import { CreateThreadInput } from './dto/create-thread.input';
 import { CurrentUser } from 'src/auth/user.decorator';
 import { UserDataForToken } from 'src/auth/auth-types';
+import { GetThreadsArgs } from './dto/get-threads.args';
 
 @Resolver(() => Thread)
 export class ThreadsResolver {
@@ -18,5 +19,14 @@ export class ThreadsResolver {
     @CurrentUser() user: UserDataForToken,
   ) {
     return this.threadsService.createThread(createThreadInput, user._id);
+  }
+
+  @Query(() => [Thread], { name: 'threads' })
+  @UseGuards(GqlGuard)
+  getThreads(
+    @Args() getThredsArgs: GetThreadsArgs,
+    @CurrentUser() user: UserDataForToken,
+  ) {
+    return this.threadsService.getThreads(getThredsArgs, user._id);
   }
 }
--- src/chats/threads/threads.service.ts
+++ src/chats/threads/threads.service.ts
@@ -3,10 +3,12 @@ import { ChatRepository } from '../chats.repository';
 import { CreateThreadInput } from './dto/create-thread.input';
 import { Thread } from './entities/thread.entity';
 import { Types } from 'mongoose';
+import { GetThreadsArgs } from './dto/get-threads.args';
 
 @Injectable()
 export class ThreadsService {
   constructor(private readonly chatRepository: ChatRepository) {}
+
   async createThread({ content, chatId }: CreateThreadInput, userId: string) {
     const thread: Thread = {
       _id: new Types.ObjectId(),
@@ -27,5 +29,14 @@ export class ThreadsService {
       },
     );
     return thread;
+  }
+
+  async getThreads({ chatId }: GetThreadsArgs, userId: string) {
+    return (
+      await this.chatRepository.findOne({
+        _id: chatId,
+        $or: [{ creatorId: userId }, { memberIds: { $in: [userId] } }],
+      })
+    ).threads;
   }
 }
